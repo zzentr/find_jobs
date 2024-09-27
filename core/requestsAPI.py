8a2d049079aad69038166b743facce2a31152ee2
zzentr <kiselevant0n@mail.ru>
2024-09-27 16:21:49 +1000

    функция которая отправляет вакансии по специальности

--- core/requestsAPI.py
+++ core/requestsAPI.py
@@ -1,11 +1,11 @@
 import aiohttp
 
-async def search_vacanties(speciality):
+async def search_vacancies(speciality):
     async with aiohttp.ClientSession() as session:
         params = {
             'host': 'hh.ru',
             'text': 'python',
-            'per_page': 10,
+            'per_page': 20,
             'locale': 'RU'
         }
         async with session.get('https://api.hh.ru/vacancies?', params=params) as response:
--- core/states/search.py
+++ core/states/search.py
@@ -1,8 +1,12 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from asyncio import to_thread
+
 from core.states.imports import *
-from core.handlers.requestsAPI import search_vacanties
-import pprint
+from core.requestsAPI import search_vacancies
+from core.keyboards.reply import more_vacancies
 
 router = Router()
+all_vacancies = {}
 
 
 class Search(StatesGroup):
@@ -12,8 +16,55 @@ class Search(StatesGroup):
 
 @router.message(Search.speciality)
 async def send_vacancies(message: Message, state: FSMContext):
-    response = await search_vacanties(message.text)
-    pprint.pprint(response)
-    # await message.answer(str(response))
-    await message.answer('ладнт')
+    response = await search_vacancies(message.text)
+    message_vacancies = await to_thread(create_message_search, response, message.from_user.id)
+    await message.answer(message_vacancies, parse_mode='html', reply_markup=more_vacancies, disable_web_page_preview=True)
     await state.clear()
+
+def create_message_search(response: dict, tg_id: int) -> str:
+    vacancies = response['items']
+    currencies = {'RUB': 'руб.', 'RUR': 'руб.', 'KZT': 'тенге', 'EUR': 'евро', 'USD': '$'}
+    message = '👁 Чтобы посмотреть подробную информацию о вакансии, напишите мне её номер!\n\n'
+
+    for i, vacancy in enumerate(vacancies):
+        
+        id = vacancy['id']
+        name = vacancy['name']
+        salary = ''
+        area = f'🗺 {vacancy['area']['name']}'
+        employer_name = vacancy['employer']['name']
+        snippet = ''
+        url = f'✔️ Откликнуться: {vacancy['alternate_url']}'
+
+        dash = '\n--------------------------\n\n'
+        if i == 4:
+            dash = ''
+
+        if vacancy['salary']:
+
+            if vacancy['salary']['from']:
+                salary = f'\n{vacancy['salary']['from']} '
+
+            if vacancy['salary']['to']:
+                if salary:
+                    salary += f'- {vacancy['salary']['to']} '
+                else:
+                    salary = f'до {vacancy['salary']['to']} '
+
+            salary += currencies[vacancy['salary']['currency']]
+
+        if vacancy['snippet']['responsibility']:
+            snippet += (f'Обязанности: {vacancy['snippet']['responsibility'].replace('<highlighttext>', '').replace('</highlighttext>', '')}\n\n')
+            
+        if vacancy['snippet']['requirement']:
+            snippet += (f'Требования: {vacancy['snippet']['requirement'].replace('<highlighttext>', '').replace('</highlighttext>', '')}\n\n')
+        
+        
+
+        all_vacancies[tg_id] = {i+1: {'id': id, 'name': name, 'salary': salary, 'area': area, 'employer_name': employer_name,
+                                    'snippet': snippet, 'url': url}}
+        if i >= 5:
+            continue
+        message += f'<b>{i+1}. {name}</b>\n{salary}\n{area} - {employer_name}\n\n{snippet}{url}{dash}'
+        
+    return message
